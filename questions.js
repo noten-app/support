ae7b0c01d0e9623bdb13296bb1b6adabe10328a4
Konstantin Protzen <code@kondev.de>
2023-09-10 00:24:28 +0200

    🎉 Add questions

---
+++ questions.js
@@ -0,0 +1,44 @@
+function hideAll() {
+    const q = document.getElementsByClassName("questions");
+    for (var i = 0; i < q.length; i++) {
+        q[i].classList.remove("question")
+        q[i].style.display = "none";
+    }
+    const a = document.getElementsByClassName("inputs");
+    for (var i = 0; i < a.length; i++) {
+        a[i].classList.remove("input")
+        a[i].style.display = "none";
+    }
+}
+
+const questions = document.getElementsByClassName("questions");
+const inputs = document.getElementsByClassName("inputs");
+
+let currentQuestion = "root";
+
+document.getElementById("control-submit").addEventListener("click", () => {
+    const currentVal = document.getElementById("input-" + currentQuestion).value;
+    hideAll();
+    const nextQuestion = document.getElementById("question-" + currentVal);
+    nextQuestion.classList.add("question");
+    nextQuestion.style.display = "block";
+    const nextInput = document.getElementById("input-" + currentVal);
+    nextInput.classList.add("input");
+    nextInput.style.display = "block";
+    currentQuestion = currentVal;
+});
+
+hideAll();
+document.getElementById("question-root").classList.add("question");
+document.getElementById("question-root").style.display = "block";
+document.getElementById("input-root").classList.add("input");
+document.getElementById("input-root").style.display = "block";
+
+// Back
+document.getElementById("control-back").addEventListener("click", () => {
+    hideAll();
+    document.getElementById("question-root").classList.add("question");
+    document.getElementById("question-root").style.display = "block";
+    document.getElementById("input-root").classList.add("input");
+    document.getElementById("input-root").style.display = "block";
+});
