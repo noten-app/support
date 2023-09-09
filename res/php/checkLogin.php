ffcf3009dc0b577b1bef98ae35132d1b31ae57da
Konstantin Protzen <code@kondev.de>
2023-09-10 01:07:06 +0200

    ➕ Add login checker script

---
+++ res/php/checkLogin.php
@@ -0,0 +1,10 @@
+<?php 
+
+    // Not logged in
+    function checkLogin(){
+        return !(
+            !isset($_SESSION['login_method']) ||
+            !isset($_SESSION['user_name']) ||
+            !isset($_SESSION['user_id'])
+        );
+    }
